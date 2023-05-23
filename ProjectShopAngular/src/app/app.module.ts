d8a3ac35781e61124fb108597c880d6d1d827ecd
phuocnehihi1 <130758054+phuocnehihi1@users.noreply.github.com>
2023-05-23 17:24:41 +0700

    create-from product

--- ProjectShopAngular/src/app/app.module.ts
+++ ProjectShopAngular/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    ProductListComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
