48ee69d5ee1f322a3ab9267dbdda6a13f096d1d3
phuocnehihi1 <130758054+phuocnehihi1@users.noreply.github.com>
2023-05-24 10:31:02 +0700

    @ngModule in module .ts

--- ProjectShopAngular/src/app/components/product-list/product-list.component.ts
+++ ProjectShopAngular/src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/common/product';
 
 @Component({
   selector: 'app-product-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+
+  Products: Product[] =[
+  new Product('P01','Hat'),
+  new Product('P02','T-Shirt'),
+  new Product('P03','Jearn')
+
+  ]
 
   constructor() { }
 
