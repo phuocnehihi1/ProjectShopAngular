f8844ca25a6121b7b220e9e221b7a9288274146e
phuocnehihi1 <130758054+phuocnehihi1@users.noreply.github.com>
2023-05-25 10:15:10 +0700

    submitproductditail

--- ProjectShopAngular/src/app/app.module.ts
+++ ProjectShopAngular/src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductdetailComponent } from './components/productdetail/productdetail.component';
+import { FormsModule } from '@angular/forms';
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductdetailComponent
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    // Khai FormsModule để sử dụng tow-wayBiding
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
