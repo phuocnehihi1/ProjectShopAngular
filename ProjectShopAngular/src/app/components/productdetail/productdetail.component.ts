83fbfc2d49865f2f318aefe3ef3425929497dbba
phuocnehihi1 <130758054+phuocnehihi1@users.noreply.github.com>
2023-05-25 10:19:08 +0700

    create star

--- ProjectShopAngular/src/app/components/productdetail/productdetail.component.ts
+++ ProjectShopAngular/src/app/components/productdetail/productdetail.component.ts
@@ -1,4 +1,8 @@
+
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/common/product';
+import { Form, NgForm } from '@angular/forms';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-productdetail',
@@ -7,7 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductdetailComponent implements OnInit {
 
-  constructor() { }
+  product: Product;
+  constructor() {
+    // Khỏi tạo class Product
+    this.product = new Product('','',0,0,0);
+  }
+
+  changeDetail(form:NgForm){
+    console.log(form.value);
+    this.product.productCode = form.value.productCode;
+    this.product.productName = form.value.productName;
+    this.product.productQuatiy = form.value.productQuatity;
+    this.product.productPrice =  form.value.productPrice;
+    this.product.productDiscount = form.value.productDiscount;
+  }
 
   ngOnInit() {
   }
